66e3170c63697702b405b58d7910ff2e180e74a7
taozun <1135978748@qq.com>
2017-06-20 16:00:53 +0800

    123

--- test/src/com/tz/test/MyTest2.java
+++ test/src/com/tz/test/MyTest2.java
@@ -1,14 +1,14 @@
 package com.tz.test;
 
 public class MyTest2 {
-	public static void main(String[] args) {
-		System.out.println(111);
-	}
+    public static void main(String[] args) {
+        System.out.println(111);
+    }
 
-	public static void master() {
-	}
+    public static void master() {
+    }
 
-	public static void dev() {
-		System.out.println(1342346);
+    public static void dev() {
+        System.out.println(146);
 	}
 }
